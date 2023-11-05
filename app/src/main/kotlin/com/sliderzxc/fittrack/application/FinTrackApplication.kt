36b8c820fa4ffa9ff052b391d0a60881300b6b38
vadwwxz <vadwwxz@gmail.com>
2023-11-05 17:14:03 +0200

    update gradle in project, create app entry

---
+++ app/src/main/kotlin/com/sliderzxc/fittrack/application/FinTrackApplication.kt
@@ -0,0 +1,11 @@
+package com.sliderzxc.fittrack.application
+
+import android.app.Application
+
+class FinTrackApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
